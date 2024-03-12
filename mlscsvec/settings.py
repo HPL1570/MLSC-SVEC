2c9c492ba264d833ff957ff7b2deef9d307b26de
HPL1570 <hemanthpatcha55555@gmail.com>
2024-03-13 00:12:39 +0530

    second4

--- mlscsvec/settings.py
+++ mlscsvec/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-rrp_p1fk!%_(dpvd+#ojs96h#13mqcf&#0g9phctb69#tz!cc&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
