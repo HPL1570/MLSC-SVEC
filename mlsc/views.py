954e3d199c5ed44a2dd7b4e89b0bc60902086285
Vinay Siddha <121566763+VinaySiddha@users.noreply.github.com>
2024-03-13 14:12:35 +0530

    Update views.py to templaet

--- mlsc/views.py
+++ mlsc/views.py
@@ -51,23 +51,30 @@ def storeDetails(request):
 # def sentmail(name,email,rolln,con,clg):
 def sendIn(request):
     if request.method == 'POST':
-        fn = request.POST['name']
-        email = request.POST['mail']
-        rolln = request.POST['roll']
-        branch = request.POST['branch']
-        event = request.POST['event']
-        if EventRe.objects.filter(rollnumber=rolln).exists() or EventRe.objects.filter(email=email).exists():
-            print("EMAIL OR ROLLNUMBER ALREADY REGISTERED")
-            error_message = 'EMAIL OR ROLLNUMBER ALREADY EXSISTED'
-            return render(request, 'index.html', {'error_message': error_message})
-        else:
-            error_message='YOUR REGISTRATION IS COMPLETED THANK YOU PLEASE CHECK YOUR MAIL FROM MLSC IF NOT FOUND CHECK IN JUNK MAIL'
-            obj = EventRe.objects.create(name=fn, email=email, rollnumber=rolln,branch=branch,event=event)
-            print("Registration")
-            sendmail(name=fn, email=email,rollno=rolln,branch=branch,event=event)
-            obj.save();
+        try:
+            fn = request.POST['name']
+            email = request.POST['mail']
+            rolln = request.POST['roll']
+            branch = request.POST['branch']
+            event = request.POST['event']
             
-    return render(request, 'index.html', {'error_message': error_message})
+            if EventRe.objects.filter(rollnumber=rolln).exists() or EventRe.objects.filter(email=email).exists():
+                print("EMAIL OR ROLLNUMBER ALREADY REGISTERED")
+                error_message = 'EMAIL OR ROLLNUMBER ALREADY EXISTS'
+                return render(request, 'index.html', {'error_message': error_message})
+            else:
+                error_message = 'YOUR REGISTRATION IS COMPLETED. THANK YOU. PLEASE CHECK YOUR MAIL FROM MLSC. IF NOT FOUND, CHECK IN JUNK MAIL'
+                obj = EventRe.objects.create(name=fn, email=email, rollnumber=rolln, branch=branch, event=event)
+                print("Registration")
+                sendmail(name=fn, email=email, rollno=rolln, branch=branch, event=event)
+                obj.save()
+                return render(request, 'index.html', {'error_message': error_message})
+        except Exception as e:
+            print("An error occurred:", e)
+            # Log the error or handle it appropriately
+            error_message = 'An error occurred while processing your request'
+            return render(request, 'index.html', {'error_message': error_message})
+
 def sendmail(name=None, email=None, rollno=None, branch=None, event=None):
     subject = "Invitation from MLSC-SVEC workshop"
     html_message = render_to_string('email.html', {'name': name, 'rollno': rollno, 'branch': branch, 'event': event})
